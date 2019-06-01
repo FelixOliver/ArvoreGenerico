9baae8cad4eec571b085f2c8d5ac6add0a0740bb
Sumari <oliver.sumari@isabo.ai>
2019-05-31 23:38:47 -0300

    insere no arvore e imprimir

---
+++ agenerico.h
@@ -0,0 +1,162 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+/*
+tipo ->
+* 1 = CIR , 2 = QUA, 3 = RET, 4 = TRA, 5 = TRI
+
+*/
+typedef struct info
+{
+    int tipo;
+    int * dados;
+    int size_d;
+    float area;
+}TINF;
+// 
+
+
+// cria info y calcula seu area
+// os dados 12 3 4 , b B h
+
+TINF * ini_info()
+{
+    TINF * novo = (TINF *)malloc(sizeof(TINF));
+    
+    return novo;
+}
+
+TINF * cria_info(TINF * inf,int tipo, int * dados)
+{
+    //TINF * inf = (TINF *)malloc(sizeof(TINF));
+    inf->tipo = tipo;
+    inf->dados = dados;
+    switch (tipo)
+    {
+    case 1:
+           inf->size_d = 1;
+           inf->area =  M_PI * powf(dados[0], 2.0 );      
+        break;
+    case 2:
+           inf->size_d = 1;
+           inf->area = dados[0] * dados[0];
+        break;
+    case 3:
+            inf->size_d = 2;
+            inf->area = dados[0] * dados[1];
+        break;
+    case 4:
+            inf->size_d = 3;
+            inf->area = (dados[1]+dados[0]) * dados[2] / 2.0;
+        break;
+    case 5:
+            inf->size_d = 2;
+            inf->area = dados[0] * dados[1];
+        break;    
+    default:
+        break;
+    }
+    return inf;
+
+}
+
+
+/*******************************************/
+
+typedef struct ag{
+    int cod;
+    TINF * info;
+    struct ag * filho;
+    struct ag * prox_irmao;
+}TAG;
+
+/** tecnicas de AG**/
+
+TAG * ini_ag()
+{
+    TAG * novo = (TAG *)malloc(sizeof(TAG));    
+    novo->cod = 0;
+    novo->filho = NULL;
+    novo->prox_irmao = NULL;
+    novo->info = NULL;
+    return novo;
+}
+
+TAG * insere(TAG * ag, int cod, int cod_pai, int tipo, int * dados){
+    if(ag == NULL)
+    {    
+        //printf("entro if !ag \n");   
+        if(cod_pai == 0)
+        {
+            //root
+            ag = ini_ag();
+            ag->cod = cod;
+            ag->info = ini_info();
+            ag->info = cria_info(ag->info, tipo, dados);
+            
+            printf("Creo ARBOL y inserto: %d \n",ag->cod);
+            //return ag;
+        }else{
+            ag = NULL;
+            printf("Retornare NULL\n");
+        }  
+    }
+    else if(ag->cod == cod_pai )
+    {   
+        TAG * novo = ini_ag();
+        novo->cod = cod;
+        novo->info = ini_info();
+        novo->info = cria_info(novo->info, tipo, dados);
+
+        if(ag->filho == NULL)
+        {
+            ag->filho = novo;
+            printf("Agrego hijo %d a %d \n",ag->filho->cod,ag->cod);
+            //return ag;
+        }
+        else
+        {
+            TAG * p = ini_ag();
+            p = ag->filho;
+            printf("antes de while p filho: %d\n",p->cod);
+            while (p->prox_irmao != NULL) p = p->prox_irmao;
+            
+            printf("antes de isertar ultimo irmao: %d\n",p->cod);
+            
+            p->prox_irmao = novo;
+            
+            printf("inserto hermano -> %d \n",p->prox_irmao->cod);
+        }
+        //return ag;
+    }
+    else if(ag->filho != NULL || ag->prox_irmao != NULL )
+    {
+        printf("Ingreso a funcion para inserar en hijo\n");
+        ag->filho =  insere(ag->filho, cod, cod_pai, tipo, dados);
+        printf("retorne de hijo \n");
+        printf("Ingreso a funcion para inserar al hermano\n");
+        ag->prox_irmao =  insere(ag->prox_irmao, cod, cod_pai, tipo, dados);
+        printf("retorne de hermano \n");
+    }
+    
+    return ag;
+}
+
+void print_ag(TAG * a)
+{
+    if(a == NULL)
+    {
+        return;
+    }
+
+    print_ag(a->filho);
+    printf("NODO %d \n",a->cod);
+    if(a->filho != NULL)
+        printf("nodo %d ->hijo-> %d\n",a->cod ,a->filho->cod);
+    print_ag(a->prox_irmao);
+    if(a->prox_irmao != NULL)
+        printf("nodo %d ->irmao-> %d\n",a->cod ,a->prox_irmao->cod);
+    return;
+}
---
+++ main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "agenerico.h"
+
+/*
+tipo ->
+* 1 = CIR , 2 = QUA, 3 = RET, 4 = TRA, 5 = TRI
+
+*/
+
+/*
+1/0/TRI 3 2
+2/1/RET 3 3
+3/1/TRA 2 3 4
+4/1/CIR 4
+5/4/QUA 3
+6/2/RET 2 2
+7/5/TRA 1 2 3
+8/5/CIR 2
+9/4/QUA 1
+10/1/TRI 1 2
+
+*/
+
+int main(){
+    //char * ex = (char *)malloc(sizeof(char)*10);
+    //ex = "23";
+    //int *p;
+    //p = pt_dados("23 43 1",3);
+    int * ss = (int *)malloc(sizeof(int));
+    ss[0] = 3;
+    TINF * th = ini_info();
+    th = cria_info(th, 2, ss);
+    //printf("a:%f , t:%d , l:%d \n", th->area, th->tipo, th->dados[0]);
+    int uno[2] = {3,2};
+    int dos[2] = {3,3};
+    int tres[3] = {2,3,4};
+    int cuatro[1] = {4};
+    int cinco[1] = {3};
+    int seis[2] = {2,2};
+    int siete[3] = {1,2,3};
+    int ocho[2] = {2};
+    int nueve[1] = {1};
+    int diez[2] = {1,2};
+
+    TAG * prin = NULL;
+    //prin = ini_ag();
+    //prin->cod = 2;
+    //printf("%d", !prin);
+    prin = insere(prin,1,0,5,uno);
+    printf("--------  \n");
+    prin = insere(prin,2,1,3,dos);
+    //printf("2- %d \n",prin->cod);
+    printf("--------  \n");
+    
+    prin = insere(prin,3,1,4,tres);
+    prin = insere(prin,4,1,1,cuatro);
+    prin = insere(prin,5,4,2,cinco);
+    prin = insere(prin,6,2,3,seis);
+    prin = insere(prin,7,5,4,siete);
+    prin = insere(prin,8,5,1,ocho);
+    prin = insere(prin,9,4,2,nueve);
+    prin = insere(prin,10,1,5,diez);
+
+    printf("hijo de root -> %d \n", prin->filho->cod);
+    //printf("%d \n",prin->prox_irmao->cod);
+    printf("-----------PRINT ARVORE-------------\n");
+    print_ag(prin);
+
+    return 0;
+}
